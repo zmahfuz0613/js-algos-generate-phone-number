9d9d016f47ac39c7375e4cb69107474fe5df4788
Andre Pato <anpato1994@gmail.com>
2020-02-04 20:06:36 -0500

    initialize repo

---
+++ phone.js
@@ -0,0 +1,9 @@
+/**
+ * @function createPhoneNumber should create a valid US phone number and return it as a string.
+ *
+ * @param {nums} nums may be an array or a number, your algorithm should compensate for both cases.
+ */
+
+const createPhoneNumber = nums => {}
+
+module.exports = createPhoneNumber
