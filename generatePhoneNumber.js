c4d0144463b5530d488b3c65bd04068acaf0d1bf
zmahfuz061318 <str053mgr@gmail.com>
2020-08-01 23:00:32 -0400

    Completed

--- generatePhoneNumber.js
+++ generatePhoneNumber.js
@@ -2,7 +2,20 @@
 
 const generatePhoneNumber = nums => {
 
+  let maskValidPhoneNumber = '999-999-9999';
+  let phoneNumber = [];
+  if(Array.isArray(nums)) {
+      phoneNumber.push(...Array.from(nums));       
+  }else{
+      phoneNumber.push(...Array.from(nums.toString()));        
+  }
 
+  phoneNumber.map(function(number){
+      maskValidPhoneNumber = maskValidPhoneNumber.replace('9', number);
+  });
+
+  return maskValidPhoneNumber;
+  
 }
 
 module.exports = generatePhoneNumber
